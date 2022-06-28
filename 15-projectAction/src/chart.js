4ae6ebfb73ab2bcefc61cf1677ba697817e9e001
Thiago <bliscosque@yahoo.com.br>
2022-06-27 21:58:51 -0500

    chart

--- 15-projectAction/src/chart.js
+++ 15-projectAction/src/chart.js
@@ -1,14 +1,43 @@
 const alpha = require('alphavantage')({key:'IKB5O5JM9AT7KR9P'})
+const Chart=require('chart.js')
 
 
 function showData(symbol) {
     document.getElementById('chart').innerHTML='<button class="button is-loading">loading</button>'
     alpha.data.daily(symbol).then(data=> {
         let tempDataStore = []
+        let datesData= []
         let tSeries=data['Time Series (Daily)']
         for (let tempData in tSeries) {
             tempDataStore.push(tSeries[tempData]['4. close'])
+            datesData.push(tempData)
         }
-        document.getElementById('chart').innerHTML=tempDataStore[0]
+        //document.getElementById('chart').innerHTML=tempDataStore[0]
+
+        var ctx=document.getElementById('myChart').getContext('2d')
+        if (window.bar != undefined) window.bar.destroy()
+        window.bar = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: datesData.reverse(),
+                datasets: [{
+                    label: symbol,
+                    data: tempDataStore.reverse(),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            }
+        }) 
+
+        document.getElementById("chart").innerHTML = '<button class="button is-warning" onclick="notify(\'' + symbol + '\')">Notification</button>' 
     })
+}
+
+function notify(symbol) {
+    alert(symbol)
 }
