0b267ddc8107aa3356dd7ef444515cb9c33c6ef2
Thiago Bliscosque Goncalves <bliscosque@yahoo.com.br>
2022-07-22 11:34:55 -0500

    connectionGenerator - improvements

--- 00-Thiago-App/Thiago-App/src/connectionsGenerator-csv.js
+++ 00-Thiago-App/Thiago-App/src/connectionsGenerator-csv.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const papa = require("papaparse");
 const { PassThrough } = require('stream');
+require ('dotenv').config({path: path.resolve(__dirname, '..','.env')})
 let obsidian_path=process.env.OBSIDIAN_PATH||'/home/thiago/obsidian';
 let folderDest=path.resolve(obsidian_path,'3-Ericsson', 'conScripts')
 const fName=obsidian_path+"/3-Ericsson/connections.csv"
@@ -41,6 +42,11 @@ function generateFile(row) {
     let send4=row.Send4;
     let exp5=row.Exp5;
     let send5=row.Send5;
+    let exp6=row.Exp6;
+    let send6=row.Send6;
+    let exp7=row.Exp7;
+    let send7=row.Send7;
+    
     //console.log(cliente, pais, UID, ssgPass, serverName, exp1, send1, exp2, send2, exp3, send3, exp4, send4, exp5, send5);
 
     const fileName=`${cliente}_${pais}_${serverName}.sh`;
@@ -80,6 +86,16 @@ function generateFile(row) {
     if (exp5!=='' && send5!=='') {
         content+=`expect "${exp5}"
     send "${send5}\\r"
+    `;
+    }
+    if (exp6!=='' && send6!=='') {
+        content+=`expect "${exp6}"
+    send "${send6}\\r"
+    `;
+    }
+    if (exp7!=='' && send7!=='') {
+        content+=`expect "${exp7}"
+    send "${send7}\\r"
     `;
     }
     content+=`interact`;
